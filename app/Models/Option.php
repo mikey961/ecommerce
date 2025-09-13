8239290e6d46a78849910db4b5b3393e5db151b0
Mikey_Cerdas <mcerdas1804@gmail.com>
2025-09-13 13:06:15 -0600

    first commit

---
+++ app/Models/Option.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Option extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'type'
+    ];
+
+    //Relacion muchos a muchos
+    public function products(){
+        return $this->belongsToMany(Product::class)
+                    ->withPivot('value')
+                    ->withTimestamps();
+    }
+
+    //Relación uno a muchos 
+    public function features(){
+        return $this->hasMany(Feature::class);
+    }
+}
